5cf75cacdec2c4e4c48302be6550d8cc38f4636b
KumJungMin <rmawjdals@naver.com>
2023-03-01 19:08:12 +0900

    chore: add node-fetch for @octokit/core

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,12 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  define: {
+    global: "window",
+  },
+  resolve: {
+    alias: {
+      'node-fetch': 'isomorphic-fetch',
+    },
+  },
 })
